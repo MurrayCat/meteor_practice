4de1df691f2d89d3e8d11a2e1235cfe11e6a85ce
MurrayCat <murray.catto@yahoo.co.za>
2014-11-14 10:58:31 +0200

    clean up

--- meteor_practice/meteor_practice.js
+++ meteor_practice/meteor_practice.js
@@ -1,74 +1,56 @@
 Tasks = new Mongo.Collection("tasks");
 if (Meteor.isClient) {
-// This code only runs on the client
+
   Template.body.helpers({
-  tasks: function () {
-  if (Session.get("hideCompleted")) {
-  // If hide completed is checked, filter tasks
-  return Tasks.find({checked: {$ne: true}}, {sort: {createdAt: -1}});
-  } else {
-  // Otherwise, return all of the tasks
-  return Tasks.find({}, {sort: {createdAt: -1}});
-  }
-  },
-  hideCompleted: function () {
-  return Session.get("hideCompleted");
-  },
-  incompleteCount: function () {
-  return Tasks.find({checked: {$ne: true}}).count();
-  }
-  });
-  Template.body.events({
-  "submit .new-task": function (event) {
-  // This function is called when the new task form is submitted
-  var text = event.target.text.value;
-  Meteor.call("addTask", text);
-  // Clear form
-  event.target.text.value = "";
-  // Prevent default form submit
-  return false;
-  },
-  "change .hide-completed input": function (event) {
-  Session.set("hideCompleted", event.target.checked);
-  }
-  });
-  Template.task.events({
-  "click .toggle-checked": function () {
-  // Set the checked property to the opposite of its current value
-  Meteor.call("setChecked", this._id, ! this.checked);
-  },
-  "click .delete": function () {
-  Meteor.call("deleteTask", this._id);
-  }
-  });
-  Accounts.ui.config({
-  passwordSignupFields: "USERNAME_ONLY"
-  });
-  Tasks = new Mongo.Collection("tasks");
-  if (Meteor.isClient) {
-  // This code only runs on the client
-  Template.body.helpers({
-  tasks: function () {
-  if (Session.get("hideCompleted")) {
-  // If hide completed is checked, filter tasks
-  return Tasks.find({checked: {$ne: true}}, {sort: {createdAt: -1}});
-  } else {
-  // Otherwise, return all of the tasks
-  return Tasks.find({}, {sort: {createdAt: -1}});
-  }
-  },
-  hideCompleted: function () {
-  return Session.get("hideCompleted");
-  },
-  incompleteCount: function () {
-  return Tasks.find({checked: {$ne: true}}).count();
-  }
+  
+    tasks: function () {
+      if (Session.get("hideCompleted")) {
+      return Tasks.find({checked: {$ne: true}}, {sort: {createdAt: -1}});
+      } 
+      else {
+        return Tasks.find({}, {sort: {createdAt: -1}});
+      }
+    },
+
+    hideCompleted: function () {
+      return Session.get("hideCompleted");
+    },
+    
+    incompleteCount: function () {
+      return Tasks.find({checked: {$ne: true}}).count();
+    }
   });
   
-  Accounts.ui.config({
-    passwordSignupFields: "USERNAME_ONLY"
+  Template.body.events({
+  
+    "submit .new-task": function (event) {
+      var text = event.target.text.value;
+      Meteor.call("addTask", text);
+      event.target.text.value = "";
+    
+      return false;
+    },
+  
+    "change .hide-completed input": function (event) {
+      Session.set("hideCompleted", event.target.checked);
+    }
+    });
+  
+  Template.task.events({
+    "click .toggle-checked": function () {
+    // Set the checked property to the opposite of its current value
+      Meteor.call("setChecked", this._id, ! this.checked);
+    },
+    
+    "click .delete": function () {
+      Meteor.call("deleteTask", this._id);
+    }
   });
-  }
+   
+  Accounts.ui.config({
+   passwordSignupFields: "USERNAME_ONLY"
+  });
+  
 }
 Meteor.methods({
   addTask: function (text) {
