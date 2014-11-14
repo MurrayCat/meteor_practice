0c56daedf7b2444ef673c23e011e37497340b529
MurrayCat <murray.catto@yahoo.co.za>
2014-11-14 11:19:06 +0200

    removed autopublish and subscribed to tasks

--- meteor_practice/meteor_practice.js
+++ meteor_practice/meteor_practice.js
@@ -1,13 +1,16 @@
 Tasks = new Mongo.Collection("tasks");
 if (Meteor.isClient) {
-
+// This code only runs on the client
+  Meteor.subscribe("tasks");
   Template.body.helpers({
-  
+
     tasks: function () {
       if (Session.get("hideCompleted")) {
+      // If hide completed is checked, filter tasks
       return Tasks.find({checked: {$ne: true}}, {sort: {createdAt: -1}});
       } 
       else {
+      // Otherwise, return all of the tasks
         return Tasks.find({}, {sort: {createdAt: -1}});
       }
     },
@@ -16,41 +19,45 @@ if (Meteor.isClient) {
       return Session.get("hideCompleted");
     },
     
-    incompleteCount: function () {
+    incompleteTasks: function () {
       return Tasks.find({checked: {$ne: true}}).count();
     }
+
   });
-  
+
   Template.body.events({
-  
+
     "submit .new-task": function (event) {
+      // This function is called when the new task form is submitted
       var text = event.target.text.value;
       Meteor.call("addTask", text);
+      // Clear form
       event.target.text.value = "";
-    
+      // Prevent default form submit
       return false;
     },
-  
+
     "change .hide-completed input": function (event) {
       Session.set("hideCompleted", event.target.checked);
     }
-    });
-  
+  });
+
   Template.task.events({
+
     "click .toggle-checked": function () {
     // Set the checked property to the opposite of its current value
       Meteor.call("setChecked", this._id, ! this.checked);
     },
-    
+
     "click .delete": function () {
       Meteor.call("deleteTask", this._id);
     }
   });
-   
+
   Accounts.ui.config({
-   passwordSignupFields: "USERNAME_ONLY"
+    passwordSignupFields: "USERNAME_ONLY"
   });
-  
+
 }
 Meteor.methods({
   addTask: function (text) {
@@ -73,3 +80,8 @@ Meteor.methods({
     Tasks.update(taskId, { $set: { checked: setChecked} });
   }
 });
+if (Meteor.isServer) {
+  Meteor.publish("tasks", function () {
+    return Tasks.find();
+  });
+}
